49b920e9d4dc575205b928e58bb05eee859b7f54
pescarualex <pescarualexflorin@gmail.com>
2025-12-03 08:05:21 +0200

    created tests for IBAN generation method

--- src/test/java/IBANServiceImplTests.java
+++ src/test/java/IBANServiceImplTests.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import exceptions.CounterExceededException;
+import model.Bank;
+import org.junit.jupiter.api.Test;
+import service.impl.BankServiceImpl;
+import service.impl.IBANServiceImpl;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class IBANServiceImplTests {
+
+    @Test
+    void verifyLengthOfIBANIs28Characters() throws CounterExceededException {
+        BankServiceImpl bankService = new BankServiceImpl();
+        IBANServiceImpl ibanServiceImpl = new IBANServiceImpl();
+
+        Bank BT = new Bank("BT", "BTRL20", "BTRO", "Mastercard");
+        bankService.addBank(BT);
+
+        String iban = ibanServiceImpl.generateIBAN(BT.getID());
+        assertEquals(28, iban.length());
+    }
+
+
 }
