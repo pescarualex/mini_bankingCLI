e20494521746be772a65596343178ba5b3c74798
pescarualex <pescarualexflorin@gmail.com>
2026-01-01 08:51:55 +0200

    starting working on accountServiceImpl

--- src/main/java/service/impl/AccountServiceImpl.java
+++ src/main/java/service/impl/AccountServiceImpl.java
@@ -1,16 +1,19 @@
 package service.impl;
 
+import exceptions.CounterExceededException;
 import model.Account;
 
 public class AccountServiceImpl {
 
     BankServiceImpl bankService = new BankServiceImpl();
+    //IBANServiceImpl ibanService = new IBANServiceImpl();
+    //CardServiceImpl = cardService = new CardServiceImpl();
 
-    public Account createAccount(String bankID){
+    public Account createAccount(String bankID) {
         String bankCode = bankService.getBankCode(bankID);
         Account account = new Account(bankCode);
-//        account.setCard();
-//        account.setIban();
+//        account.setCard(cardService.generateCardNumber());
+//        account.setIban(ibanService.generateIBAN(bankID));
         return account;
     }
 }
