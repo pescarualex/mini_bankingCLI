4cbe73d07b5c3b2862454cdb3f38697bd10c9d93
pescarualex <pescarualexflorin@gmail.com>
2026-04-02 10:14:22 +0300

    added role and status for users

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -13,18 +13,7 @@ public class Main {
 
     public static void main(String[] args) throws CounterExceededException, SQLException {
 
-        ClientServiceIImpl clientServiceImpl = new ClientServiceIImpl();
-        Client client = clientServiceImpl.createClient();
-        AccountServiceImpl.createAccount(client);
-        List<AuditTrail> auditTrailByClientID = AuditTrailDAO.getAuditTrailByClientID(client.getId());
-        for (AuditTrail auditTrail : auditTrailByClientID){
-            System.out.println(auditTrail);
-        }
-        System.out.println("::::::::::");
-        List<AuditTrail> allAuditTrail = AuditTrailDAO.getAllAuditTrail();
-        for (AuditTrail auditTrail : allAuditTrail){
-            System.out.println(auditTrail);
-        }
+
 
 
     }
